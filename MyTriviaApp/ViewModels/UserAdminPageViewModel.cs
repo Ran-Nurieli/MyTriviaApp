7e02fc4665bf0c36be0aaee521658cbfd6a89bcd
User <User@LAB3-3>
2024-03-06 11:24:44 +0200

    add command- make them work in the xaml page-collectionview

--- MyTriviaApp/ViewModels/UserAdminPageViewModel.cs
+++ MyTriviaApp/ViewModels/UserAdminPageViewModel.cs
@@ -14,18 +14,73 @@ namespace MyTriviaApp.ViewModels
     internal class UserAdminPageViewModel:ViewModelBase
     {
         TriviaService triviaService;
+
+
+        private List<Player> fullList;
+        private Player selectedPlayer;
+        private Rank selectedRank;
+        public Player SelectedPlayer { get => selectedPlayer; set { selectedPlayer = value;OnPropertyChanged(); } }
+        public ICommand ClearPlayersCommand { get; private set; }
+        public ICommand LoadPlayersCommand { get; private set; }
+        public ICommand AddPlayerCommand { get; private set; }
+        public ICommand RemovePlayerCommand { get; private set; }
+        public ICommand ResetPointsCommand { get; private set; }
+        public ICommand RefreshCommand { get; private set; }
+        public ICommand FilterCommand { get; private set; }
+        public ICommand ClearFilterCommand { get; private set; }
+        public Rank SelectedRank { get => selectedRank; set { selectedRank = value; OnPropertyChanged(); } }
+
+        public ObservableCollection<Player> Players { get; set; }
+       
+
+        private bool isRefreshing;
+        public bool IsRefreshing { get => isRefreshing; set { isRefreshing = value; OnPropertyChanged(); } }
+
         public UserAdminPageViewModel(TriviaService triviaService)
         {
             this.triviaService = triviaService;
             Players = new ObservableCollection<Player>();//רשימה ריקה
             fullList = new List<Player>();
 
-            DeleteCommand = new Command(async (object obj) => { Player p = (Player)obj; Players.Remove(p); fullList.Remove(p); await triviaService.RemovePlayer(p); });//מחיקת התלמיד מהרשימה
+            RemovePlayerCommand = new Command(async (object obj) => { Player p = (Player)obj; Players.Remove(p); fullList.Remove(p); await triviaService.RemovePlayer(p); });//מחיקת התלמיד מהרשימה
             LoadPlayersCommand = new Command(async () => await LoadPlayers());
-            ClearPlayersCommand = new Command(ClearPlayers, () => Players.Count > 0);
+            FilterCommand = new Command(() =>
+            {
+                try
+                {
+                    var FilterByRanks = fullList.Where(x => x.Rank == selectedRank).ToList();
+
+                    Players.Clear();
+
+                    foreach (var student in FilterByRanks)
+                        Players.Add(student);
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            }, () => fullList != null && fullList.Count > 0);
+            ClearFilterCommand = new Command(async () => { await LoadPlayers(); }, () => fullList != null && fullList.Count > 0);
         }
 
+        private async Task LoadPlayers()
+        {
+            IsRefreshing = true;
 
+            fullList = await triviaService.GetPlayers(); 
+            Players.Clear();
+            foreach (var player in fullList)
+                Players.Add(player);
+            
+            ((Command)ClearPlayersCommand).ChangeCanExecute();
+            IsRefreshing = false;
+        }
+
+        //ריקון רשימת התלמידים המוצגת במסך
+        private void ClearPlayers()
+        {
+            Players.Clear();
+            ((Command)ClearPlayersCommand).ChangeCanExecute();
+
+        }
         public List<string> GetAllPlayers()
         {
             List<Player> players = triviaService.GetPlayers1();
@@ -38,48 +93,13 @@ namespace MyTriviaApp.ViewModels
 
         }
 
-
-        private List<Player> fullList;
-        
-        public ICommand ClearPlayersCommand { get; private set; }
-        public ICommand LoadPlayersCommand { get; private set; }
-        public ICommand AddPlayerCommand { get; private set; }
-        public ICommand RemovePlayerCommand { get; private set; }
-        public ICommand DeleteCommand { get; private set; } 
-        public ICommand ResetPointsCommand { get; private set; }
-        public ICommand RefreshCommand { get; private set; }
-        public ObservableCollection<Player> Players { get; set; }
-       
-
-        private bool isRefreshing;
-        public bool IsRefreshing { get => isRefreshing; set { isRefreshing = value; OnPropertyChanged(); } }
-
-
-
-
-
-        private async Task LoadPlayers()
+        private void Delete()
         {
-            IsRefreshing = true;//נפעיל את אייקון הרענון
-
-            fullList = await triviaService.GetPlayers();//נביא את אוסף התלמידים
-            //נעדכן את אוסף התלמידים המוצג במסך מהרשימה המלאה
-            Players.Clear();
-            foreach (var student in fullList)
-                Players.Add(student);
-            //נעדכן האם ניתן להפעיל את הפעולות
-            ((Command)ClearPlayersCommand).ChangeCanExecute();
-            IsRefreshing = false;//בסיום נבטל את אייקון הרענון
+            if(SelectedPlayer != null)
+            {
+                Players.Remove(SelectedPlayer);
+                SelectedPlayer = null;
+            }
         }
-
-        //ריקון רשימת התלמידים המוצגת במסך
-        private void ClearPlayers()
-        {
-            Players.Clear();
-            ((Command)ClearPlayersCommand).ChangeCanExecute();
-
-        }
-
-
     }
 }
